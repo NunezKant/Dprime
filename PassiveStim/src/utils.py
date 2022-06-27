3460fcf73d2eb89b14f7365feaa013c8d8c39e3c
Miguel Nunez <nunezm@hhmi.org>
2022-06-26 21:57:26 -0400

    Dprime decoder accuracy over all pairs

--- PassiveStim/src/utils.py
+++ PassiveStim/src/utils.py
@@ -5,6 +5,7 @@ import os
 from scipy import io, stats
 import numpy as np
 from suite2p.extraction import dcnv
+from scipy import ndimage
 
 
 def add_exp(database, mname, expdate, blk):
@@ -27,7 +28,7 @@ def add_exp(database, mname, expdate, blk):
     db : list
         Updated List of experiments.
     """
-    database.append({"mname": mname, "expdate": expdate, "block": blk})
+    database.append({"mname": mname, "datexp": expdate, "blk": blk})
     return database  # Return the updated list
 
 
@@ -198,11 +199,9 @@ def get_neurons_atframes(timeline, spks):
     return neurons_atframes, subset_stim
 
 
-def get_tuned_neurons(
-    neurons_atframes, subset_stim, samples_per_category=4, n_categories=2
-):
+def get_tuned_neurons(neurons_atframes, subset_stim):
     """
-    Gets the tuned neurons by computing the average of response for half of the stimuli presented,
+    Gets the tuned neurons by computing the average of response for half of the two first categories presented,
     and then correlating to the average of the neurons for the other half.
 
     Parameters
@@ -221,24 +220,27 @@ def get_tuned_neurons(
         (correlation between stimulus subsets) consistency of the neurons
     """
     unique_stimuli = np.unique(subset_stim)
-    total_samples = n_categories * samples_per_category
-    avg_response = np.zeros((2, total_samples, neurons_atframes.shape[0]), "float32")
-    for stimuli in unique_stimuli[:total_samples]:
-        selected_stimuli = np.where(subset_stim == stimuli)[0]
-        even_idx = selected_stimuli[0::2]
-        odd_idx = selected_stimuli[1::2]
-        assert (
-            len(selected_stimuli) >= 2
-        ), "Not enough stimuli to compute the mean for this category"
-        avg_response[0, stimuli - 1] = neurons_atframes[:, even_idx].mean(-1)
-        avg_response[1, stimuli - 1] = neurons_atframes[:, odd_idx].mean(-1)
-    z_avg_response = stats.zscore(avg_response, axis=1)
-    csig = (z_avg_response[0] * z_avg_response[1]).mean(0)
-    return csig
+    avg_response = np.zeros(
+        (2, len(unique_stimuli), neurons_atframes.shape[0]), "float32"
+    )
+    k = 0
+    s2 = np.zeros((2, len(unique_stimuli), neurons_atframes.shape[0]), "float32")
+    for stim in unique_stimuli:
+        ix = np.where(subset_stim == stim)[0]
+        if len(ix) >= 2:
+            avg_response[0, k] = neurons_atframes[:, ix[0]]
+            avg_response[1, k] = neurons_atframes[:, ix[1]]
+            k += 1
+    avg_response = avg_response[:, :k]
+    ss = avg_response - np.mean(avg_response, 1)[:, np.newaxis, :]
+    ss = ss / np.mean(ss ** 2, 1)[:, np.newaxis, :] ** 0.5
+
+    csig = (ss[0] * ss[1]).mean(0)
+    return avg_response, csig
 
 
 def get_decoder_stimuli(
-    neurons_atframes, subset_stim, samples_per_category=4, selected_category=0,
+    neurons_atframes, subset_stim, samples_per_category=4, train_exemplar=0,
 ):
     """
     Builds the decoder input array
@@ -251,7 +253,7 @@ def get_decoder_stimuli(
         Stimuli before recording ends
     samples_per_category: int
         Number of samples per category.
-    selected_category: int
+    train_exemplar: int
         Selected category to use as training
     Returns:
     ----------
@@ -264,23 +266,25 @@ def get_decoder_stimuli(
     _, stimulus_counts = np.unique(subset_stim, return_counts=True)
     nreps = np.min(
         stimulus_counts[
-            cats_idx[selected_category] : cats_idx[selected_category + 1]
+            cats_idx[train_exemplar] : cats_idx[train_exemplar + 1]
             + samples_per_category
         ]
     )
     decoder_stimuli = np.zeros(
         (
-            cats_idx[selected_category + 1] + samples_per_category,
+            cats_idx[train_exemplar + 1] + samples_per_category,
             neurons_atframes.shape[0],
             nreps,
         )
     )
     for stimuli in unique_stimuli[
-        : cats_idx[selected_category + 1] + samples_per_category
+        : cats_idx[train_exemplar + 1] + samples_per_category
     ]:
         selected_stimuli = np.where(subset_stim == stimuli)[0][:nreps]
         decoder_stimuli[stimuli - 1] = neurons_atframes[:, selected_stimuli]
-    return decoder_stimuli
+    X_train = decoder_stimuli[:, :, ::2]
+    X_test = decoder_stimuli[:, :, 1::2]
+    return X_train, X_test
 
 
 class DprimeDecoder:
@@ -288,13 +292,11 @@ class DprimeDecoder:
     """
     Implements a naive decoder based on the dprime separation of the neuron responses
     """
-    def __init__(
-        self, n_categories, samples_per_category=4, selected_category=0, threshold=0.5
-    ):
+
+    def __init__(self, samples_per_category=4, train_exemplar=0, threshold=0.5):
         DprimeDecoder.threshold = threshold
-        DprimeDecoder.n_categories = n_categories
         DprimeDecoder.samples_per_category = samples_per_category
-        DprimeDecoder.selected_category = selected_category
+        DprimeDecoder.train_exemplar = train_exemplar
         DprimeDecoder.mu_ = None
         DprimeDecoder.sd_ = None
         DprimeDecoder.dprime_ = None
@@ -305,21 +307,20 @@ class DprimeDecoder:
         """
         Trains with even trials
         """
-        categories = np.arange(
-            0, self.n_categories * self.samples_per_category, self.samples_per_category
-        )
-        cats_idx = (
-            categories[self.selected_category],
-            categories[self.selected_category + 1],
-        )
-        self.mu_ = X[:, :, ::2].mean(-1)
-        self.sd_ = X[:, :, ::2].std(-1)
+        self.mu_ = X.mean(-1)
+        self.sd_ = X.std(-1)
         self.dprime_ = (
             2
-            * (self.mu_[cats_idx[0] : cats_idx[1]] - self.mu_[cats_idx[1] :])
-            / (self.sd_[cats_idx[0] : cats_idx[1]] + self.sd_[cats_idx[1] :])
+            * (
+                self.mu_[: self.samples_per_category]
+                - self.mu_[self.samples_per_category :]
+            )
+            / (
+                self.sd_[: self.samples_per_category]
+                + self.sd_[self.samples_per_category :]
+            )
         )
-        self.neurons_abvtresh_ = self.dprime_[self.selected_category] > self.threshold
+        self.neurons_abvtresh_ = self.dprime_[self.train_exemplar] > self.threshold
         return self
 
     def test(self, X, iplane, zstack=1):
@@ -327,19 +328,188 @@ class DprimeDecoder:
         Tests with odd trials
         """
         if zstack == 1:
-            ix1 = (self.dprime_[self.selected_category] > self.threshold) * (
-                iplane < 10
-            )
-            ix2 = (-self.dprime_[self.selected_category] > self.threshold) * (
-                iplane < 10
-            )
+            ix1 = (self.dprime_[self.train_exemplar] > self.threshold) * (iplane < 10)
+            ix2 = (-self.dprime_[self.train_exemplar] > self.threshold) * (iplane < 10)
         else:
-            ix1 = (self.dprime_[self.selected_category] > self.threshold) * (
+            ix1 = (self.dprime_[self.train_exemplar] > self.threshold) * (
                 iplane >= 10
             )  # the last ten ROIs are in the top plane
-            ix2 = (-self.dprime_[self.selected_category] > self.threshold) * (
-                iplane >= 10
-            )
+            ix2 = (-self.dprime_[self.train_exemplar] > self.threshold) * (iplane >= 10)
         # spop is the decoder applied to test trials
-        self.spop_ = X[:, ix1, 1::2].mean(1) - X[:, ix2, 1::2].mean(1)
-        return self
+        self.spop_ = X[:, ix1, :].mean(1) - X[:, ix2, :].mean(1)
+        return self.spop_
+
+
+def load_exp_info(db):
+    """
+    Loads the experiment info from the database
+
+    Parameters:
+    ----------
+    db : dict
+        Dictionary with the database entries
+    Returns:
+    ----------
+    Timeline : dict
+        Dictionary with the experiment info
+    """
+    mname, datexp, blk = db["mname"], db["datexp"], db["blk"]
+    root = os.path.join("Z:/data/PROC", mname, datexp, blk)
+
+    fname = "Timeline_%s_%s_%s" % (mname, datexp, blk)
+    fnamepath = os.path.join(root, fname)
+
+    Timeline = io.loadmat(fnamepath, squeeze_me=True)["Timeline"]
+
+    return Timeline
+
+
+def baselining(ops, tlag, F, Fneu):
+    """
+    Baseline the neural data before deconvolution
+
+    Parameters:
+    ----------
+    ops : dict
+        Dictionary with the experiment info
+    tlag : int
+        Time lag for the deconvolution
+    F : array
+        Deconvolved fluorescence
+    Fneu : array
+        Neurophil fluorescence
+    Returns:
+    ----------
+    F : array
+        Baselined deconvolved fluorescence
+    """
+    F = preprocess(F, Fneu, ops["win_baseline"], ops["sig_baseline"], ops["fs"])
+    # F = dcnv.preprocess(F, ops['baseline'], ops['win_baseline'], ops['sig_baseline'],
+    #                   ops['fs'], ops['prctile_baseline'])
+    if tlag < 0:
+        F[:, 1:] = (1 + tlag) * F[:, 1:] + (-tlag) * F[:, :-1]
+    else:
+        F[:, :-1] = (1 - tlag) * F[:, :-1] + tlag * F[:, 1:]
+    return F
+
+
+def preprocess(F, Fneu, win_baseline, sig_baseline, fs):
+    """
+    Preprocess the fluorescence data
+
+    Parameters:
+    ----------
+    F : array
+        Deconvolved fluorescence
+    Fneu : array
+        Neurophil fluorescence
+    baseline : int
+        Baseline for the fluorescence
+    win_baseline : int
+        Window for the baseline
+    sig_baseline : int
+        Sigma for the baseline
+    fs : int
+        Sampling rate
+    Returns:
+    ----------
+    F : array
+        Preprocessed deconvolved fluorescence
+  """
+    win = int(win_baseline * fs)
+
+    Flow = ndimage.gaussian_filter(F, [0.0, sig_baseline])
+    Flow = ndimage.minimum_filter1d(Flow, win)
+    Flow = ndimage.maximum_filter1d(Flow, win)
+    F = F - 0.7 * Fneu
+    Flow2 = ndimage.gaussian_filter(F, [0.0, sig_baseline])
+    Flow2 = ndimage.minimum_filter1d(Flow2, win)
+    Flow2 = ndimage.maximum_filter1d(Flow2, win)
+
+    Fdiv = np.maximum(10, Flow.mean(1))
+    F = (F - Flow2) / Fdiv[:, np.newaxis]
+
+    return F
+
+
+### Load the neural data
+def load_neurons(db, dual_plane=True, baseline=True):
+    """
+    Loads the neural data from the database
+
+    Parameters:
+    ----------
+
+    db : dict
+        Dictionary with the database entries
+    dual_plane : Boolean
+        Dual plane flag indicates whether the data is from the dual plane or not
+    Baseline : Boolean
+        Baseline flag indicates whether the data is preproceded or not
+    Returns:
+    ----------
+    spks : array
+        Spike matrix
+        
+    """
+    mname, datexp, blk = db["mname"], db["datexp"], db["blk"]
+    root = os.path.join("Z:/data/PROC", mname, datexp, blk)
+    ops = np.load(
+        os.path.join(root, "suite2p", "plane0", "ops.npy"), allow_pickle=True
+    ).item()
+
+    if dual_plane:
+        tlags = np.linspace(0.2, -0.8, ops["nplanes"] // 2 + 1)[:-1]
+        tlags = np.hstack((tlags, tlags))
+        tlags = tlags.flatten()
+    else:
+        tlags = np.linspace(0.2, -0.8, ops["nplanes"] + 1)[:-1]
+    print(tlags.shape)
+
+    spks = np.zeros((0, ops["nframes"]), np.float32)
+    stat = np.zeros((0,))
+    iplane = np.zeros((0,))
+    xpos, ypos = np.zeros((0,)), np.zeros((0,))
+
+    for n in range(ops["nplanes"]):
+        ops = np.load(
+            os.path.join(root, "suite2p", "plane%d" % n, "ops.npy"), allow_pickle=True
+        ).item()
+
+        stat0 = np.load(
+            os.path.join(root, "suite2p", "plane%d" % n, "stat.npy"), allow_pickle=True
+        )
+        ypos0 = np.array([stat0[n]["med"][0] for n in range(len(stat0))])
+        xpos0 = np.array([stat0[n]["med"][1] for n in range(len(stat0))])
+
+        ypos0 += ops["dy"]
+        xpos0 += ops["dx"]
+
+        if baseline:
+            F = np.load(os.path.join(root, "suite2p", "plane%d" % n, "F.npy"))
+            Fneu = np.load(os.path.join(root, "suite2p", "plane%d" % n, "Fneu.npy"))
+            F = baselining(ops, tlags[n], F, Fneu)
+            spks0 = dcnv.oasis(F, ops["batch_size"], ops["tau"], ops["fs"])
+        else:
+            spks0 = np.load(
+                os.path.join(root, "suite2p", "plane%d" % n, "spks.npy"),
+                allow_pickle=True,
+            )
+
+        spks0 = spks0.astype("float32")
+        if spks.shape[1] > spks0.shape[0]:
+            spks0 = np.concatenate(
+                (spks0, np.zeros((spks0.shape[0], spks.shape[1] - spks0.shape[1]))),
+                axis=1,
+            )
+        spks = np.concatenate((spks, spks0.astype("float32")), axis=0)
+        ypos = np.concatenate((ypos, ypos0), axis=0)
+        xpos = np.concatenate((xpos, xpos0), axis=0)
+        iplane = np.concatenate((iplane, n * np.ones(len(stat0),)))
+        stat = np.concatenate((stat, stat0), axis=0)
+
+        print("plane %d, " % n, "neurons: %d" % len(xpos0))
+
+    print("total neurons %d" % len(spks))
+
+    return spks, xpos, ypos, iplane, stat
